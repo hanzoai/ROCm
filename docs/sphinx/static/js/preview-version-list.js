1fed1af074fa98211a288d4469821ce51ccb5441
Peter Park <peter.park@amd.com>
2025-09-12 15:18:43 -0400

    update docs config

--- docs/sphinx/static/js/preview-version-list.js
+++ docs/sphinx/static/js/preview-version-list.js
@@ -11,8 +11,8 @@ ready(() => {
 		"a.header-all-versions[href='https://rocm.docs.amd.com/en/latest/release/versions.html']",
 	);
 	versionListLink.textContent = "Preview versions"
-	versionListLink.href = "https://rocm.docs.amd.com/en/docs-7.0-rc1/preview/versions.html"
+	versionListLink.href = "https://rocm.docs.amd.com/en/docs-7.0-rc/preview/versions.html"
 
 	const headerLogoText = document.querySelector("div.header-logo a:not(.navbar-brand):not(.header-all-versions)")
-	headerLogoText.textContent = "ROCm™ Software 7.0 RC1"
+	headerLogoText.textContent = "ROCm™ Software 7.0 release candidate"
 });
