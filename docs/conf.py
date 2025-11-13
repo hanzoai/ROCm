1138cdb21092cf7ae049545e0c1c3d1a00fe8c64
Alex Xu <alex.xu@amd.com>
2025-11-13 15:57:09 -0500

    add preview announcement

--- docs/conf.py
+++ docs/conf.py
@@ -150,6 +150,7 @@ extensions = [
 external_projects_current_project = "rocm"
 html_theme = "rocm_docs_theme"
 html_theme_options = {
+    "announcement": "This is ROCm 7.9.0 technology preview release documentation. For production use, refer to <a id='rocm-banner' href='https://rocm.docs.amd.com/en/latest/'>ROCm 7.1.0 documentation</a>.",
     "flavor": "generic",
     "header_title": "ROCm™ 7.9.0 Preview",
     "header_link": "https://rocm.docs.amd.com/en/7.9.0-preview/index.html",
